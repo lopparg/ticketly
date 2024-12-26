260b4f2a965c71b3309931c3772ae2e751bcca82
Rustam Isaev <lopparg@gmail.com>
2024-12-26 15:15:11 +0100

    add java doc to status

--- src/main/java/com/lopparg/ticketly/ticket/Status.java
+++ src/main/java/com/lopparg/ticketly/ticket/Status.java
@@ -1,5 +1,26 @@
 package com.lopparg.ticketly.ticket;
 
+/**
+ * Enum representing the possible status values for a ticket in the system.
+ * <p>
+ * A ticket can be in one of the following statuses:
+ * <ul>
+ *     <li>{@link Status#OPEN} - The ticket is open and active, awaiting resolution.</li>
+ *     <li>{@link Status#CLOSED} - The ticket has been resolved and is now closed.</li>
+ * </ul>
+ * </p>
+ * <p>
+ * This enum helps in tracking the state of tickets within the system, making it easier to filter and manage tickets based on their status.
+ * </p>
+ */
 public enum Status {
-    CLOSED, OPEN
+    /**
+     * The ticket is open and still requires attention.
+     */
+    CLOSED,
+
+    /**
+     * The ticket has been resolved and is now closed.
+     */
+    OPEN
 }
