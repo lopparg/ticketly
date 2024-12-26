96c891fca057fc0e6e7a5aeafe527e2eafc5ef6e
Rustam Isaev <lopparg@gmail.com>
2024-12-26 14:02:19 +0100

    Add open API configuration

---
+++ src/main/java/com/lopparg/ticketly/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.lopparg.ticketly;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Ticketly API")
+                        .version("1.0.0")
+                        .description("Ticketly API description"));
+    }
+}
